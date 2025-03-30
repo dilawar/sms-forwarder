e80e9126a32b7d5b286fe59164866d8d277a08b5
Dilawar Singh <dilawar@dognosis.tech>
2025-03-31 05:24:28 +0530

    Adds dummy plugin

--- src-capacitor/android/app/src/main/java/com/dilawar/capplugins/SmsPlugin.java
+++ src-capacitor/android/app/src/main/java/com/dilawar/capplugins/SmsPlugin.java
@@ -1,21 +1,19 @@
 package com.dilawar.capplugins;
 
+import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
-import org.json.JSONObject;
-
 @CapacitorPlugin(name = "SmsPlugin")
 public class SmsPlugin extends Plugin {
 
-    @Override
-    @PluginMethod
+    @PluginMethod()
     public void echo(PluginCall call) {
         String value = call.getString("value");
 
-        JSONObject ret = new JSONObject();
+        JSObject ret = new JSObject();
         ret.put("value", value);
         call.resolve(ret);
     }
