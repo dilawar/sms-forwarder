2f154a9151f66bd16766a6285dfde342eff98f3c
Dilawar Singh <dilawar@dognosis.tech>
2025-03-31 08:49:09 +0530

    Plugin is receiving SMSs

--- src-capacitor/android/app/src/main/java/com/dilawar/capplugins/SmsPlugin.java
+++ src-capacitor/android/app/src/main/java/com/dilawar/capplugins/SmsPlugin.java
@@ -1,5 +1,9 @@
 package com.dilawar.capplugins;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 
 import com.getcapacitor.JSObject;
@@ -12,6 +16,19 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 public class SmsPlugin extends Plugin {
 
     private final String TAG = "sms_plugin";
+    private BroadcastReceiver receiver;
+
+    public void onCreate() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.dilawar.cppplugins.ACTION_RECV_SMS");
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "received ACTION_RECV_SMS"+intent);
+            }
+        };
+    }
 
     @PluginMethod()
     public void echo(PluginCall call) {
